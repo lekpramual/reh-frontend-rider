bb964fdbf89df93acf2ef2da08b7e8642651f4db
pramual nadtayai <lek.pramual@gmail.com>
2024-09-30 16:10:32 +0700

    feat: set lavel data

--- src/app/core/services/menu.service.ts
+++ src/app/core/services/menu.service.ts
@@ -29,7 +29,7 @@ export class MenuService {
   }
 
   public menuManager(){
-    const levelData = parseInt('6');
+    const levelData = parseInt('0');
 
     console.log(levelData);
     if(levelData === 6){
